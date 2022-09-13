e72d0cd68739f9dad55b783eef0c7adc302821ac
Matt Elliott <eliotmat@gmail.com>
2022-09-13 08:38:49 -0400

    added some stuff from channels

--- codeFromChannels.js
+++ codeFromChannels.js
@@ -1,5 +1,84 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: false
+}))
+const cors = require('cors');
+// const axios = require('axios');
+// const fetch = require("isomorphic-fetch");
+app.use(cors());
+// const HTMLParser = require('node-html-parser');
+const fs = require('fs');
+const myJson = require('./download.json');
+const bookmarks = require('./cheap_pvt.json');
+const https = require('https');
+app.use(express.static('public'));
+let urlFetch = 'https://www.thisthing.com';
+
+async function getObject(){
+    return await fetch('https://www.thisthing.com')
+}
+
+const filterByReference = (arr1, arr2) => {
+   let res = [];
+   res = arr1.filter(el => {
+      return arr2.find(element => {
+         return element.username == el.name;
+      });
+   });
+   return res;
+}
+
+
+function writeAFile(content) {
+    console.log('trying to writeFile');
+    fs.writeFileSync('./channels.json', JSON.stringify(content));
+}
+
+function appendChannels(channel) {
+    bookmarks.push(channel);
+    writeAFile(bookmarks);
+}
+
+// GET THE ACTUAL CHANNELS FROM CB
+async function fetchJson(){
+    let fetchedJson = await fetch('https://www.thisthing.com');
+    let theJson = await fetchedJson.json();
+    //console.log(theJson);
+    return await theJson;
+}
+
+async function updateChannelMetadata(){
+    let allChans = await fetchJson();
+    // let chans = [];
+    let dateStamp = new Date();
+    for(let i = 0; i < bookmarks.length; i++){
+        let thatIndex = allChans.findIndex(object => {
+            return object.username == bookmarks[i].name;
+        });
+        if(thatIndex != -1){
+            bookmarks[i].lastDetected = `${dateStamp.getFullYear()}/${dateStamp.getMonth()+1}/${dateStamp.getDate()}`;
+            if(allChans[thatIndex].image_url){
+                bookmarks[i].imgSrc = allChans[thatIndex].image_url;
+                console.log(`found image for ${bookmarks[i].name}`)
+            }
+            else{
+                bookmarks[i].imgSrc = "";
+                console.log(`no image found for ${bookmarks[i].name}`);
+            }
+            //console.log(bookmarks[i]);
+            //console.log(that[thatIndex]);N
+            console.log(bookmarks[i]);
+            writeAFile(bookmarks);
+            //chans.push(bookmarks[i]);
+        }    
+    }
+    // writeAFile(JSON.stringify(chans));
+    return bookmarks;
+
 app.get('/api/getJson', async (req, res) => {
-    let myChan = await fetch('https://chaturbate.com/affiliates/api/onlinerooms/?format=json&wm=1oPNc');
+    let myChan = await fetch('https://www.thisthing.com');
     let bookmarksImgs = await updateChannelMetadata();
     myChan = await myChan.json();
     let onlineOnes = await filterByReference(bookmarksImgs, myChan);
